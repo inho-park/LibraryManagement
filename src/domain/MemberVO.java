db8a869d4ec1949dcad71d985ebd56702e74fa8e
inho-park <ttotto0608@naver.com>
2022-11-25 01:03:55 +0900

    modify: MemberVO append Getter, Setter

--- src/domain/MemberVO.java
+++ src/domain/MemberVO.java
@@ -9,4 +9,44 @@ public class MemberVO {
     private String phone;
 
     private String bno_list;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getBno_list() {
+        return bno_list;
+    }
+
+    public void setBno_list(String bno_list) {
+        this.bno_list = bno_list;
+    }
 }
