601f182f3c3004f763fb1cc9ec58e0bd4d88dc90
inho-park <ttotto0608@naver.com>
2022-11-24 12:14:00 +0900

    modify: BookMapper

--- src/mapper/BookMapper.java
+++ src/mapper/BookMapper.java
@@ -1,4 +1,20 @@
 package mapper;
 
-public class BookMapper {
+import domain.BookVO;
+
+import java.util.List;
+
+public interface BookMapper {
+    public List<BookVO> getList();
+
+    public void insert(BookVO board);
+
+    public void insertSelectKey(BookVO board);
+
+    public BookVO read(int bno);
+
+    public int delete(int bno);
+
+    public int update(BookVO board);
+
 }
