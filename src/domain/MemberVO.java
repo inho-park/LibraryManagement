f4e38f7f821dd8b2cc2e099256dd86bb11f19b8f
inho-park <ttotto0608@naver.com>
2022-11-24 15:06:28 +0900

    add: memberVO

---
+++ src/domain/MemberVO.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.sql.Date;
+
+public class MemberVO {
+    private String name;
+    private String id;
+    private String pw;
+    private String phone;
+
+    private String bno_list;
+}
