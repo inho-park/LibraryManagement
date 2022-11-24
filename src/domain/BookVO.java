be64fd483f3372acec61d484811e7b1ac730d185
inho-park <ttotto0608@naver.com>
2022-11-24 15:05:45 +0900

    modify: bookVO append rental,return date

--- src/domain/BookVO.java
+++ src/domain/BookVO.java
@@ -1,15 +1,18 @@
 package domain;
 
+import java.sql.Date;
 import java.util.ArrayList;
 
 public class BookVO {
+
     private ArrayList<Integer> arrGenre;
     private int bno;
     private String title;
     private String publisher;
     private String author;
     private String genre;
-
+    private Date rentalDate;
+    private Date returnDate;
 
     public int getBno() {
         return bno;
@@ -31,6 +34,14 @@ public class BookVO {
         return genre;
     }
 
+    public Date getRentalDate() {
+        return rentalDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
     public void setBno(int bno) {
         this.bno = bno;
     }
@@ -49,5 +60,13 @@ public class BookVO {
 
     public void setGenre(String genre) {
         this.genre = genre;
+    }
+
+    public void setRentalDate(Date rentalDate) {
+        this.rentalDate = rentalDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
     }
 }
